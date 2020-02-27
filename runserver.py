76eb5471c4d7ff5c4aa2d54a71d45b4e7ba5035e
MDonev <donev90@gmail.com>
2020-02-27 15:23:21 +0200

    commit

--- runserver.py
+++ runserver.py
@@ -1,10 +1,4 @@
-from os import environ
 from bmes import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run(port=80, debug=True)
